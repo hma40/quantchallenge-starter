7fd61b1d722b889ca1bded56a0516877e0c4c456
Andrew Li <andrew10securities@gmail.com>
2025-09-21 15:18:31 -0400

    full test file

--- research/data/format.java
+++ research/data/format.java
@@ -2,11 +2,11 @@ import java.util.*;
 import java.io.*;
 public class format {
   public static void main(String[] args) throws IOException {
-    BufferedReader train = new BufferedReader(new FileReader("train.csv"));
-    BufferedReader train_new = new BufferedReader(new FileReader("train_new.csv"));
-		PrintWriter pw = new PrintWriter("train_full.csv");
+    BufferedReader train = new BufferedReader(new FileReader("test.csv"));
+    BufferedReader train_new = new BufferedReader(new FileReader("test_new.csv"));
+		PrintWriter pw = new PrintWriter("test_full.csv");
 
-    for(int i = 0; i < 80001; i++) {
+    for(int i = 0; i < 15997; i++) {
       String[] orig = train.readLine().split(",");
       String[] next = train_new.readLine().split(",");
       if(next.length == 0) {
@@ -18,18 +18,16 @@ public class format {
           next[0] = "0.0";
         }
       }
-      String[] combo = new String[19];
-      for(int j = 0; j < 15; j++) {
+      String[] combo = new String[18];
+      for(int j = 0; j < 16; j++) {
         combo[j] = orig[j];
       }
-      combo[15] = next[0];
-      combo[16] = next[1];
-      combo[17] = orig[15];
-      combo[18] = orig[16];
-      for(int j = 0; j < 18; j++) {
+      combo[16] = next[0];
+      combo[17] = next[1];
+      for(int j = 0; j < 17; j++) {
         pw.print(combo[j] + ",");
       }
-      pw.println(combo[18]);
+      pw.println(combo[17]);
     }
 
     pw.close();
